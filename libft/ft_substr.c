97be9dd1b1a44f476b2493f71de425c2532811ce
jeounpar <jeounpar@student.42seoul.kr>
2021-11-07 21:20:46 +0900

    add calloc strjoin substr & fix libft.h

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -1,25 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jeounpar <jeounpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/07 16:35:48 by jeounpar          #+#    #+#             */
-/*   Updated: 2021/11/07 16:35:50 by jeounpar         ###   ########.fr       */
+/*   Created: 2021/11/07 19:21:32 by jeounpar          #+#    #+#             */
+/*   Updated: 2021/11/07 19:21:33 by jeounpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t nmemb, size_t size)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	void	*str;
-	int		i;
+	char	*tmp;
+	char	*str;
+	size_t	i;
 
-	str = (void *)malloc(nmemb * size);
+	tmp = (char *)s;
+	str = (char *)malloc((len + 1) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
-	ft_bzero(str, nmemb * size);
+	i = 0;
+	while (i < len && tmp[i] != '\0')
+	{
+		str[i] = tmp[start];
+		i++;
+		start++;
+	}
+	str[i] = '\0';
 	return (str);
 }
