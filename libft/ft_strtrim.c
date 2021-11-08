94cfe5e1a234b50bf224e991fd512fe96bb5c591
jeounpar <jeounpar@student.42seoul.kr>
2021-11-08 13:21:53 +0900

    remove memccpy & fix strtrim

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -12,66 +12,26 @@
 
 #include "libft.h"
 
-static int	is_ok(char c, char *set)
-{
-	int	i;
-
-	i = 0;
-	while (set[i] != '\0')
-	{
-		if (c == set[i])
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-static int	get_length(char *s1, char *set)
-{
-	int	i;
-	int	len;
-
-	i = 0;
-	len = 0;
-	while (s1[i] != '\0')
-	{
-		if (is_ok(s1[i], set))
-			len++;
-		i++;
-	}
-	return (len);
-}
-
-static	void	putin_str(char *s1, char *set, char *str)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	j = 0;
-	while (s1[i] != '\0')
-	{
-		if (is_ok(s1[i], set))
-		{
-			str[j] = s1[i];
-			j++;
-		}	
-		i++;
-	}
-	str[j] = '\0';
-}
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	size_t	start;
+	size_t	end;
 	char	*str;
-	char	*s1_str;
-	char	*set_str;
 
-	s1_str = (char *)s1;
-	set_str = (char *)set;
-	str = (char *)malloc((get_length(s1_str, set_str) + 1) * sizeof(char));
-	if (str == NULL)
-		return (NULL);
-	putin_str(s1_str, set_str, str);
+	str = 0;
+	if (s1 != 0 && set != 0)
+	{
+		start = 0;
+		end = ft_strlen(s1);
+		while (s1[start] && ft_strchr(set, s1[start]))
+			start++;
+		while (s1[end - 1] && ft_strchr(set, s1[end - 1]) && end > start)
+			end--;
+		str = (char *)malloc((end - start + 1) * sizeof(char));
+		if (str == NULL)
+			return (NULL);
+		else
+			ft_strlcpy(str, &s1[start], end - start + 1);
+	}
 	return (str);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -41,7 +41,6 @@ char	**ft_split(char const *s, char c);
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
-void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
 void	ft_bzero(void *s, size_t n);
