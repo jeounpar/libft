501dfc9e50f041b36040427487f4dc348465600a
jeounpar <jeounpar@student.42seoul.kr>
2021-11-16 17:00:26 +0900

    fix memchr

--- rank00/libft/ft_memchr.c
+++ rank00/libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: jeounpar <jeounpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 13:32:10 by jeounpar          #+#    #+#             */
-/*   Updated: 2021/11/16 16:58:42 by jeounpar         ###   ########.fr       */
+/*   Updated: 2021/11/16 17:00:10 by jeounpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	unsigned char	tmp;
 	unsigned char	*str;
 
-	if (c == NULL)
-		return (s);
 	i = 0;
 	tmp = c;
 	str = (unsigned char *)s;
+	if (c == NULL)
+		return (&str[0]);
 	while (str[i] != '\0' && i < n)
 	{
 		if (str[i] == tmp)
