328596edf4284e6e053aef2139f35a309df53f26
jeounpar <jeounpar@student.42seoul.kr>
2021-11-16 16:59:16 +0900

    fix memchr memcpy

--- rank00/libft/ft_memcpy.c
+++ rank00/libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jeounpar <jeounpar@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 21:51:43 by jeounpar          #+#    #+#             */
-/*   Updated: 2021/11/06 21:51:44 by jeounpar         ###   ########.fr       */
+/*   Updated: 2021/11/16 16:57:54 by jeounpar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,10 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 
 	dest_str = (unsigned char *)dest;
 	src_str = (unsigned char *)src;
+	if (n == 0 || dest_str == src_str)
+		return (dest);
+	if (dest == NULL && src == NULL)
+		return (NULL);
 	i = 0;
 	while (i < n)
 	{
